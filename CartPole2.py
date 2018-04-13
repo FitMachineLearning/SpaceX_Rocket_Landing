ee31971141f4b347b3adbc2322a80b2d48ca8b8a
Fit Machine Learning <33303615+FitMachineLearning@users.noreply.github.com>
2018-04-12 18:04:18 -0700

    Create CartPole2.py

---
+++ CartPole2.py
@@ -0,0 +1,61 @@
+import gym
+import numpy as np
+from tensorforce.agents import PPOAgent
+from tensorforce.agents import TRPOAgent
+#from tensorforce import Configuration
+
+
+NUM_GAMES_TO_PLAY = 70000
+MAX_MEMORY_LEN = 100000
+CLIP_ACTION = False
+
+env = gym.make("CartPole-v0")
+# Create a Proximal Policy Optimization agent
+#actions=dict(type='float', shape=(2,)),
+
+agent = PPOAgent(
+    states=dict(type='float32', shape=(4,)),
+    actions=dict(type='int', num_actions=2),
+    network=[
+        dict(type='dense', size=32,activation='relu'),
+        dict(type='dense', size=32,activation='relu'),
+        dict(type='dense', size=32,activation='relu')
+    ]
+)
+
+
+'''
+    batching_capacity=200,
+    step_optimizer=dict(
+        type='adadelta',
+        learning_rate=1e-3)
+'''
+
+allRewards = np.zeros(shape=(1,1))
+
+for game in range(NUM_GAMES_TO_PLAY):
+    obs = env.reset()
+    gameTotalReward = 0
+    for step in range(1000):
+        env.render()
+        a = agent.act(obs)
+        #print("ACTION ->",a)
+        if CLIP_ACTION:
+            for i in range (np.alen(a)):
+                if a[i] < -1: a[i]=-0.99999999999
+                if a[i] > 1: a[i] = 0.99999999999
+        obs, reward, done, info = env.step(a)
+        #reward = reward/100
+        gameTotalReward = gameTotalReward + reward
+        allRewards = np.vstack((allRewards, np.array([reward])))
+        if done:
+            agent.observe(reward=reward, terminal=True)
+        else:
+            agent.observe(reward=reward, terminal=False)
+
+        #print("Action: {} Observations Size:{} score: {}".format(a,obs.shape,reward))
+        if done:
+            print("#",game," last game average ", (gameTotalReward/step)*100,"recent avg", allRewards.flatten()[-10000:].mean()*100,"global avg", allRewards.mean()*100, "max",allRewards.max()*100,"min",allRewards.min()*100 , "steps", step)
+            if np.alen(allRewards) >= MAX_MEMORY_LEN:
+                allRewards = allRewards[1200:]
+            break
